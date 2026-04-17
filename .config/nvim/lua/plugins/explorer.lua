b92a6f3987b2285dfb033e1ad6ea2417accf3f63
Alec Plotkin <alp.plotkin@gmail.com>
2026-04-17 18:09:42 -0400

    Add oil.nvim plugin.

---
+++ .config/nvim/lua/plugins/explorer.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        -- Lazy loading is not recommended for oil.nvim (tricky edge cases)
+        lazy = false,
+        dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+        config = function()
+            require('oil').setup({
+                columns = {
+                    { "mtime", format = "%Y-%m-%d T%H:%M" },
+                    "icon",
+                    -- "size",
+                }
+            })
+            vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
+        end,
+    },
+    {
+        "benomahony/oil-git.nvim",
+        dependencies = { "stevearc/oil.nvim" },
+        -- No opts or config needed! Works automatically
+        config = function()
+            require("oil-git").setup({
+                highlights = {
+                    OilGitAdded = { fg = "#a6e3a1" },     -- green
+                    OilGitModified = { fg = "#f9e2af" },  -- yellow  
+                    OilGitRenamed = { fg = "#cba6f7" },   -- purple
+                    OilGitUntracked = { fg = "#89b4fa" }, -- blue
+                    OilGitIgnored = { fg = "#6c7086" },   -- gray
+                }
+            })
+        end
+    }
+}

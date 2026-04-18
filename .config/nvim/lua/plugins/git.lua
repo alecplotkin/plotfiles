b3ab17a16cada2075b79f0cd42fa24f5675310fb
Alec Plotkin <alp.plotkin@gmail.com>
2026-04-17 23:49:51 -0400

    Add keymap to vim fugitive to open git status in vertical split.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,5 +2,9 @@ return {
     'tpope/vim-fugitive',
     keys = {
         { '<leader>gs', vim.cmd.Git },
+        { '<leader>gsv', ':VG<CR>', desc = 'Git (vertical)' },
     },
+    config = function()
+        vim.api.nvim_create_user_command('VG', 'vertical Git', {})
+    end
 }

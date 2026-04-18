0ced92389fb2c74ca244f17f77952d0a33c4e318
Alec Plotkin <alp.plotkin@gmail.com>
2026-04-18 01:30:13 -0400

    Remove oil git plugin since it added too much latency; add helper-functions and disable lazy loading for vim fugitive.

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -9,28 +9,31 @@ return {
         config = function()
             require('oil').setup({
                 columns = {
-                    { "mtime", format = "%Y-%m-%d T%H:%M" },
+                    { "mtime", format = "%Y-%m-%d\t%H:%M:%S\t" },
                     "icon",
+                    -- "permissions",
                     -- "size",
                 }
             })
             vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
         end,
     },
-    {
-        "benomahony/oil-git.nvim",
-        dependencies = { "stevearc/oil.nvim" },
-        -- No opts or config needed! Works automatically
-        config = function()
-            require("oil-git").setup({
-                highlights = {
-                    OilGitAdded = { fg = "#a6e3a1" },     -- green
-                    OilGitModified = { fg = "#f9e2af" },  -- yellow  
-                    OilGitRenamed = { fg = "#cba6f7" },   -- purple
-                    OilGitUntracked = { fg = "#89b4fa" }, -- blue
-                    OilGitIgnored = { fg = "#6c7086" },   -- gray
-                }
-            })
-        end
-    }
+    -- -- Highlight git status of files. Disabled because it slows down oil.nvim
+    -- -- significantly.
+    -- {
+    --     "benomahony/oil-git.nvim",
+    --     dependencies = { "stevearc/oil.nvim" },
+    --     -- No opts or config needed! Works automatically
+    --     config = function()
+    --         require("oil-git").setup({
+    --             highlights = {
+    --                 OilGitAdded = { fg = "#a6e3a1" },     -- green
+    --                 OilGitModified = { fg = "#f9e2af" },  -- yellow  
+    --                 OilGitRenamed = { fg = "#cba6f7" },   -- purple
+    --                 OilGitUntracked = { fg = "#89b4fa" }, -- blue
+    --                 OilGitIgnored = { fg = "#6c7086" },   -- gray
+    --             }
+    --         })
+    --     end
+    -- }
 }
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,10 +1,16 @@
 return {
     'tpope/vim-fugitive',
+    lazy = false,
+    config = function()
+        vim.api.nvim_create_user_command('Gitv', function(opts)
+            vim.cmd('vertical Git ' .. (opts.args or ''))
+        end, {
+            nargs = '*',
+            complete = 'customlist,fugitive#Complete',
+        })
+    end,
     keys = {
         { '<leader>gs', vim.cmd.Git },
-        { '<leader>gsv', ':VG<CR>', desc = 'Git (vertical)' },
+        { '<leader>gsv', ':Gitv<CR>', desc = 'Git (vertical)' },
     },
-    config = function()
-        vim.api.nvim_create_user_command('VG', 'vertical Git', {})
-    end
 }

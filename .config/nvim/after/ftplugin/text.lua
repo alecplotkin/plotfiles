cc0b12e7db5e10b7f8a3adf455f9d167340d981b
Alec Plotkin <alp.plotkin@gmail.com>
2026-05-22 19:38:40 -0400

    Add ftplugin settings for txt files, update markdown ftplugin settings to match tex files.

--- .config/nvim/after/ftplugin/text.lua
+++ .config/nvim/after/ftplugin/text.lua
@@ -4,6 +4,6 @@ vim.opt_local.breakindent = true
 vim.opt_local.linebreak = true
 vim.opt_local.textwidth = 0
 vim.opt_local.showbreak = string.rep(" ", 2)
--- Remap 'j, k' for moving within word wrap 
+
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
